3683b908edc5d21864f57dd4de5f99cd80804356
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-16 11:58:28 -0430

    Directivas ngIf y Template

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  isLogged: boolean = false;
+  nivel: number = 0;
 }
